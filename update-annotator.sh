3e9f03333a7ef86fe5da935a132bd17109aead8c
brownandrew15 <54123841+brownandrew15@users.noreply.github.com>
2022-08-05 18:56:59 +0100

    The update-annotator.sh script can now update the annotator

---
+++ update-annotator.sh
@@ -0,0 +1,21 @@
+# Updates the contents of the volumes
+
+echotitle() {
+    echo "";
+    echo " ===== $1 ===== ";
+    echo "";
+}
+
+echo "+------------------------------------+";
+echo "| terms4FAIRskills Annotator Updater |";
+echo "+------------------------------------+";
+
+# Get the container ID for the web container
+echotitle "Getting Container"
+CONTAINERID=$(docker ps --filter "name=terms4FAIRskills_annotator_web" --quiet | head -n 1)
+echo "The container ID is $CONTAINERID"
+
+# Copy the host files to the web container
+echotitle "Copying the project to the container"
+docker cp ./ $CONTAINERID:/var/www/
+echo "Updated Container Contents"
