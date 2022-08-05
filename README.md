0f4cc1cf33484edfe24315316d7e9f11943c0cd3
brownandrew15 <54123841+brownandrew15@users.noreply.github.com>
2022-08-05 02:05:45 +0100

    The installer now has options to control the installation
    The -c flag sets that docker build should not use the cache when building the image, the -d flag sets that the database volumes should be removed and regenerated and the -f flag sets the containers to be run in the foreground.

--- README.md
+++ README.md
@@ -4,6 +4,11 @@ Tool for annotating materials with terms4FAIRskills ontology.
 ## Installation
 To install the annotator run `sh install-annotator.sh`.
 
+Optional Flags:
+- `-c`: Builds the Docker image without using the cache
+- `-d`: Clears the database volumes and rebuilds them
+- `-f`: Runs the containers in the foreground (opposite of `-d` in `docker compose up`)
+
 ## JavaScript Debug Logging
 The JavaScript conole output can be turned on by adding the `debug` parameter to the URL query string.
 
--- install-annotator.sh
+++ install-annotator.sh
@@ -1,33 +1,89 @@
 
+
 echotitle() {
-    echo ""
-    echo " ===== $1 ===== "
-    echo ""
+    echo "";
+    echo " ===== $1 ===== ";
+    echo "";
 }
 
-echo "+--------------------------------------+"
-echo "| terms4FAIRskills Annotator Installer |"
-echo "+--------------------------------------+"
+# Set variable defaults
+NOCACHE=0;
+FOREGROUND=0;
+CLEARDATABASE=0;
+
+# get the flags that were passed
+while getopts 'cfd:' OPTION; do
+    case "$OPTION" in
+        c)
+            # Building the Docker Image without using the cache
+            NOCACHE=1;
+            ;;
+        d)
+            # Clearing the database volumes
+            CLEARDATABASE=1;
+            ;;
+        f)
+            # Running the containers in the foreground
+            FOREGROUND=1;
+            ;;
+        ?)
+            echo "script usage: install-annotator.sh [-c] [-d] [-f]" >&2
+            exit 1
+        ;;
+    esac
+done
+shift "$(($OPTIND -1))"
+
+# Print the title
+echo "+--------------------------------------+";
+echo "| terms4FAIRskills Annotator Installer |";
+echo "+--------------------------------------+";
 
 # Clearing previous containers
-echotitle "Clearing previous containers"
-docker compose down
+echotitle "Clearing previous containers";
+docker compose down;
 
 # Clearing the web_data volume
 echotitle "Clearing terms4fairskills_web_data Volume"
 if docker volume ls --filter "name=terms4fairskillsannotator_web_data" --quiet | grep -q 'terms4fairskillsannotator_web_data'; then
-    docker volume rm terms4fairskillsannotator_web_data --force
+    docker volume rm terms4fairskillsannotator_web_data --force;
+    echo "Cleared the web_data volume";
 else
-    echo "terms4fairskills_web_data Volume doesn't exist - nothing to clear"
+    echo "terms4fairskills_web_data Volume doesn't exist - nothing to clear";
 fi
 
-# Build the docker image
-echotitle "Building Docker Image"
-docker build --no-cache .
+# Clearing the mysql_database and _mysql_data volumes
+if [ $CLEARDATABASE -eq 1 ]; then
+    # if the database volumes should also be cleared
+    echotitle "Clearing the database volumes";
+    docker volume rm terms4fairskillsannotator_mysql_data --force;
+    docker volume rm terms4fairskillsannotator_mysql_database --force;
+    echo "Cleared the database volumes"
+fi
 
-# Start the docker container
-echotitle "Starting Docker Containers"
-docker compose up -d
+
+# Build the docker image
+if [ $NOCACHE -eq 1 ]; then
+    # if the install command has the --no-cache flag
+    echotitle "Building Docker Image (without using the cache)";
+    docker build --no-cache .;
+else 
+    # if the install command does not have the no cache flag
+    echotitle "Building Docker Image";
+    docker build .;
+fi
+
+# Start the docker containers
+if [ $FOREGROUND -eq 1 ]; then
+    # if the containers should be run in the foreground
+    echotitle "Starting Docker Containers (in the foreground)";
+    docker compose up;
+else 
+    # if the containers should be run in the background
+    echotitle "Starting Docker Containers";
+    docker compose up -d;
+fi
+
 
 # Report that the installation was completed
-echotitle "Installation complete"
+echotitle "Installation complete";
